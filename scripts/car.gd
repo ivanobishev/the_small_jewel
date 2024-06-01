3accb8b254d268b7dc36756ada02535189a18a89
Ivan <donivanbecerra@gmail.com>
2024-06-01 08:42:58 +0200

    Add a car and paint roads

---
+++ scripts/car.gd
@@ -0,0 +1,12 @@
+extends VehicleBody3D
+
+var max_rpm = 500
+var max_torque = 200
+
+func _physics_process(delta):
+	steering = lerp(steering, Input.get_axis("turn_right", "turn_left") * 0.4, 5 * delta)
+	var acceleration = Input.get_axis("reverse", "accelerate")
+	var rpm = $BackLeftWheel.get_rpm()
+	$BackLeftWheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
+	rpm = $BackRightWheel.get_rpm()
+	$BackRightWheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
