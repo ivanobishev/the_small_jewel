ce56c58306aae063b60f440b6c25a3ca57bd6a9e
Ivan <donivanbecerra@gmail.com>
2024-06-09 22:54:06 +0200

    Add city hall

--- scripts/car.gd
+++ scripts/car.gd
@@ -6,6 +6,9 @@ var max_torque = 200
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("turn_right", "turn_left") * 0.4, 5 * delta)
 	var acceleration = Input.get_axis("reverse", "accelerate")
+	
+	if acceleration < 0:
+		acceleration /= 2
 	var rpm = $BackLeftWheel.get_rpm()
 	$BackLeftWheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
 	rpm = $BackRightWheel.get_rpm()
