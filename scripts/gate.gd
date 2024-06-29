6164642d0f72cd6c0e41a604dd06232f54e7126e
Ivan <donivanbecerra@gmail.com>
2024-06-29 09:44:44 +0200

    Delete .godot folder, add map limits

--- scripts/gate.gd
+++ scripts/gate.gd
@@ -1,18 +1,20 @@
 extends Node3D
 
-var closed = true
+var closed
+var current_rotation
+
+func _ready():
+	closed = true
+	current_rotation = rad_to_deg(rotation.y)
 
 func toggle():
 	var tween = create_tween()
-	var current_rotation = rad_to_deg(rotation.y)
-	
-	print(current_rotation)
 	
 	if closed:
-		tween.tween_property($".", "rotation:y", deg_to_rad(current_rotation - 90), 3)
+		tween.tween_property(self, "rotation:y", deg_to_rad(current_rotation - 90), 3)
 		closed = false
 	else:
-		tween.tween_property($".", "rotation:y", deg_to_rad(current_rotation + 90), 3)
+		tween.tween_property(self, "rotation:y", deg_to_rad(current_rotation), 3)
 		closed = true
 
 func _input(event):
